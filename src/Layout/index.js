0fd31d6b9c91097c2f1d91d99918e75016ff201f
Aaron Yang <aaronyang385@gmail.com>
2023-12-28 13:09:25 -0600

    fixed dependencies/added form component

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,47 +1,51 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { Switch, Route } from "react-router";
 import Header from "./Header";
 import NotFound from "./NotFound";
-import Home from "../Components/Home";
-import CreateDeck from "../Components/CreateDeck";
-import Deck from "../Components/Deck";
-import Study from "../Components/Study";
-import EditDeck from "../Components/EditDeck";
-import AddCard from "../Components/AddCard";
-import EditCard from "../Components/EditCard";
+import Home from "./Home";
+import DeckPage from "./Deck";
+import AddCards from "./AddCard";
+import EditCard from "./EditCard";
+import StudyPage from "./Study";
+import CreateDeck from "./CreateDeck";
+import EditDeck from "./EditDeck";
 
 function Layout() {
-    return (
-        <>
-            <Header />
-            <div className="container">
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/decks/new">
-                        <CreateDeck />
-                    </Route>
-                    <Route exact path="/decks/:deckId">
-                        <Deck />
-                    </Route>
-                    <Route path="/decks/:deckId/study">
-                        <Study />
-                    </Route>
-                    <Route path="/decks/:deckId/edit">
-                        <EditDeck />
-                    </Route>
-                    <Route path="/decks/:deckId/cards/new">
-                        <AddCard />
-                    </Route>
-                    <Route path="/decks/:deckId/cards/:cardId/edit">
-                        <EditCard />
-                    </Route>
-                    <NotFound />
-                </Switch>
-            </div>
-        </>
-    );
+  const [deck, setDeck] = useState([]);
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path={`/decks/:deckId/cards/:cardId/edit`}>
+            <EditCard deck={deck} setDeck={setDeck} />
+          </Route>
+          <Route path={`/decks/:deckId/cards/new`}>
+            <AddCards deck={deck} setDeck={setDeck} />
+          </Route>
+          <Route path={`/decks/:deckId/edit`}>
+            <EditDeck deck={deck} setDeck={setDeck} />
+          </Route>
+          <Route path={`/decks/:deckId/study`}>
+            <StudyPage deck={deck} setDeck={setDeck} />
+          </Route>
+          <Route path={`/decks/new`}>
+            <CreateDeck />
+          </Route>
+          <Route path={`/decks/:deckId`}>
+            <DeckPage deck={deck} setDeck={setDeck} />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </>
+  );
 }
 
 export default Layout;
